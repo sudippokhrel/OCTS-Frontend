4f360e3dc499e0b58bf309286636f4a389873506
Niroj Sedhai <nirojsedhai124@gmail.com>
2023-07-02 13:12:35 +0545

    SeatList page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Appbar from '../components/Appbar'
 import Box from '@mui/material/Box';
-import {Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useUserAuth } from '../components/context/UserAuthContext';
+// import {Button } from '@mui/material';
+// import { useNavigate } from 'react-router-dom';
+// import { useUserAuth } from '../components/context/UserAuthContext';
 
 import '../App.css';
 import SeatList from './Seats/SeatList';
@@ -12,8 +12,18 @@ import SeatList from './Seats/SeatList';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
-
+const Home=()=> {
+  // const { logOut, user } = useUserAuth();
+  // const navigate = useNavigate();
+  // const handleLogout = async () => {
+  //   try {
+  //     await logOut();
+  //     toast.success('You have successfully logged out')
+  //     navigate("/login");
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+  // };
 
 
 
